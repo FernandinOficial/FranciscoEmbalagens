0fdcb62a90749a50378d94b87c5c760a6016c315
FernandinOficial <fernandoribeirooficial123@gmail.com>
2024-06-14 16:22:00 -0300

    commit

--- index.php
+++ index.php
@@ -1,15 +1,38 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home - Francisco Embalagens</title>
     <link rel="stylesheet" href="style/style.css" type="text/css">
 </head>
+
 <body>
-    <header></header>
-    <nav></nav>
-    <main></main>
-    <footer></footer>
+    <header>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde nihil fuga, animi inventore, praesentium odit
+            sed nisi, aperiam tempora vero ipsum nobis quisquam ad nam? Consequuntur neque nam obcaecati quas!
+        </p>
+    </header>
+    <nav>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde nihil fuga, animi inventore, praesentium odit
+            sed nisi, aperiam tempora vero ipsum nobis quisquam ad nam? Consequuntur neque nam obcaecati quas!
+        </p>    
+    </nav>
+    <main>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde nihil fuga, animi inventore, praesentium odit
+            sed nisi, aperiam tempora vero ipsum nobis quisquam ad nam? Consequuntur neque nam obcaecati quas!
+        </p>
+    </main>
+    <footer>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde nihil fuga, animi inventore, praesentium odit
+            sed nisi, aperiam tempora vero ipsum nobis quisquam ad nam? Consequuntur neque nam obcaecati quas!
+        </p>
+    </footer>
 </body>
+
 </html>
--- login.php
+++ login.php
@@ -4,7 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="style/style.css" type="text/css">
 </head>
 <body>
+    <input type="text" name="nome" placeholder="Nome">
+    <input type="email" name="email" placeholder="Email">
+    <input type="password" name="senha" placeholder="Senha">
+    <input type="password" name="repitasenha" placeholder="Repita sua senha">
 </body>
 </html>
