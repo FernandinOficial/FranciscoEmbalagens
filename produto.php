37f737f0bbcab556de49312405a54b6301094a3e
FernandinOficial <fernando.furibeiro@gmail.com>
2024-08-15 23:09:46 -0300

    commit começando página de produtos, adicionando a tabela

--- produto.php
+++ produto.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,14 +8,41 @@
     <link rel="stylesheet" href="style/style.css" type="text/css">
     <script defer src="script/script.js"></script>
 </head>
+
 <body>
-    <?php 
-        require_once "header.php";
+    <?php
+    require_once "header.php";
     ?>
     <table border="1">
-        <th>Produtos
+        <tr>
+            <th>MODELOS</th>
+            <th>DIMENSÕES</th>
+        </tr>
+        <tr>
+            <td>P1</td>
             <td></td>
-        </th>
+        </tr>
+        <tr>
+            <td>P2</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>P3</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>P4</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>P6</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>P7</td>
+            <td></td>
+        </tr>
     </table>
 </body>
+
 </html>
