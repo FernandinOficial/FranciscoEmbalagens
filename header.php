a4ef23f98ae7990cad0583092f8e5a6af8b94852
FernandinOficial <fernando.furibeiro@gmail.com>
2024-08-16 22:46:06 -0300

    resolvendo alterações do menu navbar e carrossel

--- header.php
+++ header.php
@@ -1,33 +1,18 @@
 <nav>
-    <div id="dropdown">
-        <div id="nav-bar">
-            <div class="dropdown-button">
-                <button id="dropbtn">mostrar
-                    <img src="multimidia/" alt="">
-                </button>
-                <div class="dropdown-content">
-                    <ul>
-                        <li><a href="">OLÁ, (USER)</a></li>
-                        <li><a href="">PRODUTOS</a></li>
-                        <li><a href="">CONTATO</a></li>
-                        <li><a href="">SOBRE</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div id="logo"><img src="" alt="Logo Francisco Embalagens"></div>
-        </div>
-    </div>
-
     <!-- bootstrap -->
     <div class="dropdown">
         <button class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            Menu
         </button>
         <ul class="dropdown-menu">
-            <li><button class="dropdown-item" type="button">Dropdown item</button></li>
-            <li><button class="dropdown-item" type="button">Dropdown item</button></li>
-            <li><button class="dropdown-item" type="button">Dropdown item</button></li>
+            <li><button class="dropdown-item" type="button"><a href="">OLÁ, (USER)</a></button></li>
+            <li><button class="dropdown-item" type="button"><a href="">PRODUTOS</a></button></li>
+            <li><button class="dropdown-item" type="button"><a href="">CONTATO</a></button></li>
+            <li><button class="dropdown-item" type="button"><a href="">SOBRE</a></button></li>
         </ul>
+        <div id="logo">
+            <img src="multimidia/images/icon/franciscoLogo_100x35.png" alt="Logo Francisco Embalagens">
+        </div>
     </div>
-    
+
 </nav>
