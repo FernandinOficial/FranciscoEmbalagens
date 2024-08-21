ba1df92309a32b427793c4ced4b912663cf299e6
FernandinOficial <fernando.furibeiro@gmail.com>
2024-08-20 23:02:13 -0300

    fazendo alterações na página Header.php, tamanho, cor, e posiçoes dos elementos dos botões de navegação

--- header.php
+++ header.php
@@ -1,8 +1,8 @@
 <nav>
-    <!-- bootstrap -->
+    <!-- Bootstrap implementado-->
     <div class="dropdown">
-        <button class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Menu
+        <button id="btn-dropdown" class="btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <img src="multimidia/images/navButton_39x45.png" alt="">
         </button>
         <ul class="dropdown-menu">
             <li><button class="dropdown-item" type="button"><a href="">OLÁ, (USER)</a></button></li>
@@ -10,9 +10,8 @@
             <li><button class="dropdown-item" type="button"><a href="">CONTATO</a></button></li>
             <li><button class="dropdown-item" type="button"><a href="">SOBRE</a></button></li>
         </ul>
-        <div id="logo">
-            <img src="multimidia/images/icon/franciscoLogo_100x35.png" alt="Logo Francisco Embalagens">
-        </div>
     </div>
-
+    <div id="logo">
+        <img src="multimidia/images/icon/franciscoLogo_100x35.png" alt="Logo Francisco Embalagens">
+    </div>
 </nav>
