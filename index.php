bf94207b1d9cfff073f38ac1369c7eff2d30f583
FernandinOficial <fernando.furibeiro@gmail.com>
2024-08-21 01:00:15 -0300

    adicionando imagens no site e fazendo formatação de imagens com padrao para celular no momento

--- index.php
+++ index.php
@@ -59,16 +59,16 @@
             </a>
         </div>
         <div id="itens">
-            <img class="imgs-itens" src="multimidia/marketing/mk/1.png" alt="Sacolas Personalizadas">
+            <img class="imgs-itens" src="multimidia/marketing/mk/sacolasImagem1_380px.png" alt="Sacolas Personalizadas">
             <h4>DESIGNS ÚNICOS</h4>
             <p>Sacolas personalizadas no melhor estilo que você deseja, com qualidade e designs únicos.</p>
 
-            <img class="imgs-itens" src="multimidia/marketing/mk/1.png" alt="Anos de Serviço">
+            <img class="imgs-itens" src="multimidia/marketing/mk/sacolasImagem2_350px.png" alt="Anos de Serviço">
             <h4>ANOS DE SERVIÇO</h4>
             <p>Há mais de cinco anos, nos dedicamos a satisfazer nossos clientes com produtos e serviços de alta
                 qualidade.</p>
 
-            <img class="imgs-itens" src="multimidia/marketing/mk/1.png" alt="Estilos e Compromisso ambiental">
+            <img class="imgs-itens" src="multimidia/marketing/mk/sacolasImagem3_350px.png" alt="Estilos e Compromisso ambiental">
             <h4>ESTILO E COMPROMISSO AMBIENTAL</h4>
             <p>Sacolas lindas e sustentáveis para levar onde e quando quiser. Estilo e responsabilidade ambiental unidos
                 para seu cotidiano.</p>
