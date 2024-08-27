6088424a4b0ab6cb2378bb66b9fc0af881f62f13
FernandinOficial <fernando.furibeiro@gmail.com>
2024-08-27 15:02:04 -0300

    footer alterado e finalizado com sucesso

--- footer.php
+++ footer.php
@@ -5,9 +5,11 @@
         <h3 style="color: #ACEFAC;">CONTATO</h3>
         <div class="contato">
             <div class="whats">
+                <img src="multimidia/images/whats.png" alt="WhatsApp Foto">
                 <p>(19) 999998-9819</p>
             </div>
             <div class="email">
+            <img src="multimidia/images/gmail.png" alt="Gmail Foto">
                 <p>embalagensedesign@gmail.com</p>
             </div>
             <p class="copy">&copy; 2019 Francisco Embalagens.</p>
