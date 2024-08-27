8358d759d5c97d0d12bc2148a46410c8c7f396a6
FernandinOficial <fernando.furibeiro@gmail.com>
2024-08-27 16:31:37 -0300

    conteudo bruto realizado na parte do sobre, feito com Popup

--- header.php
+++ header.php
@@ -1,3 +1,6 @@
+<head>
+    <script defer src="script/script.js"></script>
+</head>
 <nav>
     <!-- Bootstrap implementado-->
     <div class="dropdown">
@@ -10,7 +13,7 @@
             </li>
             <li><button class="dropdown-item" type="button"><a href="produto.php">PRODUTOS</a></button></li>
             <li><button class="dropdown-item" type="button"><a href="">CONTATO</a></button></li>
-            <li><button class="dropdown-item" type="button"><a href="">SOBRE</a></button></li>
+            <li><button class="dropdown-item" onclick="togglePopup()"><a>SOBRE</a></button></li>
             <div id="account">
                 <a href="login.php">
                     <li class="account" id="login">LOGIN</li>
@@ -33,6 +36,17 @@
         <a href="#"><img src="multimidia/images/gmail.png" alt="Gmail"></a>
     </aside>
 </nav>
+<div class="popup" id="popup-1">
+    <div class="overlay"></div>
+    <div class="content">
+        <div class="close-btn" onclick="togglePopup()">&times;
+        </div>
+        <h1>Title</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo aspernatur laborum rem sed laudantium
+            excepturi veritatis voluptatum architecto, dolore quaerat totam officiis nisi animi accusantium alias
+            inventore nulla atque debitis.</p>
+    </div>
+</div>
 <aside id="home">
     <a class="home" href="index.php"><img src="multimidia/images/home_35px.png" alt="Home"></a>
 </aside>
--- script/script.js
+++ script/script.js
@@ -1,11 +1,16 @@
 //header
-var dropBtn = document.querySelector('#dropbtn');
-var dropdownContent = document.querySelector('.dropdown-content');
+// var dropBtn = document.querySelector('#dropbtn');
+// var dropdownContent = document.querySelector('.dropdown-content');
 
-dropBtn.addEventListener('click', function () {
-    if (dropdownContent.style.display === 'block') {
-        dropdownContent.style.display = 'none';
-    } else {
-        dropdownContent.style.display = 'block';
-    }
-});
+// dropBtn.addEventListener('click', function () {
+//     if (dropdownContent.style.display === 'block') {
+//         dropdownContent.style.display = 'none';
+//     } else {
+//         dropdownContent.style.display = 'block';
+//     }
+// });
+
+//POPUP SOBRE
+function togglePopup() {
+  document.getElementById("popup-1").classList.toggle("active");
+}
