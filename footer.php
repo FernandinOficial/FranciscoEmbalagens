450285b946c719a046db03c823a84d3ae475acd1
FernandinOficial <fernando.furibeiro@gmail.com>
2024-08-30 23:47:26 -0300

    realizando atualizações no rodapé (footer.php)

--- footer.php
+++ footer.php
@@ -1,22 +1,29 @@
 <footer>
-    <div id="footer-logo"><a href="#"><img src="multimidia/images/icon/franciscoLogo_200px.png"
-                alt="Logo Francisco Embalagens"></a>
+    <div id="footer-logo">
+        <a href="#">
+            <img src="multimidia/images/icon/franciscoLogo_200px.png" alt="Logo Francisco Embalagens">
+        </a>
     </div>
+
     <div id="contato">
-        <h3 style="color: #ACEFAC;">CONTATO</h3>
-        <div class="contato">
-            <div class="whats">
+        <h3>CONTATO</h3>
+
+        <div class="contato-container">
+            <div class="contato-fluid" id="whats">
                 <img src="multimidia/images/whats.png" alt="WhatsApp Foto">
                 <p>(19) 999998-9819</p>
             </div>
-            <div class="email">
+
+            <div class="contato-fluid" id="email">
                 <img src="multimidia/images/gmail.png" alt="Gmail Foto">
                 <p>embalagensedesign@gmail.com</p>
             </div>
-            <p class="copy">&copy; 2019 Francisco Embalagens.</p>
         </div>
+
+        <p class="copy">&copy; 2019 Francisco Embalagens.</p>
     </div>
 </footer>
+
 
 </body>
 
