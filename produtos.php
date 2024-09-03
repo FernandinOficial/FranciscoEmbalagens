222ceb65e186c41a779d936ab455677522d51f63
FernandinOficial <fernando.furibeiro@gmail.com>
2024-09-02 22:58:47 -0300

    pagina produtos atualizada, mas não completa, fotos necessitam ser implementadas.  Design da página esta concluida, porem sem fotos suficiente e descrições

--- produtos.php
+++ produtos.php
@@ -29,73 +29,140 @@
     ?>
 
     <main>
+        <h1 class="text-center">Sacolas Kraft</h1>
+        <div class="container_imgProdutos">
+            <img src="multimidia/marketing/produtos/produtoKraft.png" alt="sacolas kraft"
+                class="img-fluid img_produtos">
+            <img src="multimidia/marketing/produtos/produtoKraft.png" alt="modelos e tamanhos sacolas kraft"
+                class="img-fluid img_produtos">
+        </div>
+
         <table>
-            <h1>Sacolas Kraft</h1>
-            <img src="multimidia/produtos/produtoKraft.png" alt="kraft">
-            <tr>
-                <th>Modelos</th>
-                <th>Dimensões</th>
-            </tr>
-            <tr>
-                <td>P1</td>
-                <td>15cm x 11,5cm | 5cm</td>
-            </tr>
-            <tr>
-                <td>P2</td>
-                <td>22cm x 14cm | 7cm</td>
-            </tr>
-            <tr>
-                <td>P3</td>
-                <td>19cm x 23cm | 8cm</td>
-            </tr>
-            <tr>
-                <td>P4</td>
-                <td>28cm x 19cm | 8cm</td>
-            </tr>
-            <tr>
-                <td>P6</td>
-                <td>23cm x 29cm | 12cm</td>
-            </tr>
-            <tr>
-                <td>P7</td>
-                <td>32cm x 29cm | 12cm</td>
-            </tr>
+            <thead>
+                <tr>
+                    <th>Modelos</th>
+                    <th>Dimensões</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>P1</td>
+                    <td>15cm x 11,5cm | 5cm</td>
+                </tr>
+                <tr>
+                    <td>P2</td>
+                    <td>22cm x 14cm | 7cm</td>
+                </tr>
+                <tr>
+                    <td>P3</td>
+                    <td>19cm x 23cm | 8cm</td>
+                </tr>
+                <tr>
+                    <td>P4</td>
+                    <td>28cm x 19cm | 8cm</td>
+                </tr>
+                <tr>
+                    <td>P6</td>
+                    <td>23cm x 29cm | 12cm</td>
+                </tr>
+                <tr>
+                    <td>P7</td>
+                    <td>32cm x 29cm | 12cm</td>
+                </tr>
+            </tbody>
+        </table>
+
+        <br>
+        <hr class="hr_produtos">
+        <br>
+        <h1 class="text-center">Sacolas Plásticas</h1>
+        <div class="container_imgProdutos">
+            <img src="multimidia/marketing/produtos/produtoPlastico.png" alt="sacolas plásticas"
+                class="img-fluid img_produtos">
+            <img src="multimidia/marketing/produtos/produtoPlastico.png" alt="modelos e tamanhos sacolas plásticas"
+                class="img-fluid img_produtos">
+        </div>
+
+        <table>
+            <thead>
+                <tr>
+                    <th>Modelos</th>
+                    <th>Dimensões</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>P1</td>
+                    <td>15cm x 11,5cm | 5cm</td>
+                </tr>
+                <tr>
+                    <td>P2</td>
+                    <td>22cm x 14cm | 7cm</td>
+                </tr>
+                <tr>
+                    <td>P3</td>
+                    <td>19cm x 23cm | 8cm</td>
+                </tr>
+                <tr>
+                    <td>P4</td>
+                    <td>28cm x 19cm | 8cm</td>
+                </tr>
+                <tr>
+                    <td>P6</td>
+                    <td>23cm x 29cm | 12cm</td>
+                </tr>
+                <tr>
+                    <td>P7</td>
+                    <td>32cm x 29cm | 12cm</td>
+                </tr>
+            </tbody>
         </table>
         <br>
+        <hr class="hr_produtos">
         <br>
+        <h1 class="text-center">Sacolas Plásticas</h1>
+        <div class="container_imgProdutos">
+            <img src="multimidia/marketing/produtos/produtoKraft.png" alt="sacolas kraft"
+                class="img-fluid img_produtos">
+            <img src="multimidia/marketing/produtos/produtoKraftTamanhos.png" alt="modelos e tamanhos sacolas kraft"
+                class="img-fluid img_produtos">
+        </div>
+
         <table>
-            <h1>Sacolas Plásticas</h1>
-            <img src="multimidia/produtos/produtoPlastico.png" alt="plástico">
-            <tr>
-                <th>Modelos</th>
-                <th>Dimensões</th>
-            </tr>
-            <tr>
-                <td>P1</td>
-                <td>15cm x 11,5cm | 5cm</td>
-            </tr>
-            <tr>
-                <td>P2</td>
-                <td>22cm x 14cm | 7cm</td>
-            </tr>
-            <tr>
-                <td>P3</td>
-                <td>19cm x 23cm | 8cm</td>
-            </tr>
-            <tr>
-                <td>P4</td>
-                <td>28cm x 19cm | 8cm</td>
-            </tr>
-            <tr>
-                <td>P6</td>
-                <td>23cm x 29cm | 12cm</td>
-            </tr>
-            <tr>
-                <td>P7</td>
-                <td>32cm x 29cm | 12cm</td>
-            </tr>
+            <thead>
+                <tr>
+                    <th>Modelos</th>
+                    <th>Dimensões</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>P1</td>
+                    <td>15cm x 11,5cm | 5cm</td>
+                </tr>
+                <tr>
+                    <td>P2</td>
+                    <td>22cm x 14cm | 7cm</td>
+                </tr>
+                <tr>
+                    <td>P3</td>
+                    <td>19cm x 23cm | 8cm</td>
+                </tr>
+                <tr>
+                    <td>P4</td>
+                    <td>28cm x 19cm | 8cm</td>
+                </tr>
+                <tr>
+                    <td>P6</td>
+                    <td>23cm x 29cm | 12cm</td>
+                </tr>
+                <tr>
+                    <td>P7</td>
+                    <td>32cm x 29cm | 12cm</td>
+                </tr>
+            </tbody>
         </table>
-    </main>
-    <?php
-    require_once 'includes/footer.php';
-    ?>
+        <br><br>
+        <?php
+        require_once 'includes/footer.php';
+        ?>
