e45a482eeca779a809a85a6f2c4499b7972c090a
joao <00001079676673sp@al.educacao.sp.gov.br>
2024-09-19 19:41:30 -0300

    Script de banco de dados concluido

--- database/installDatabase.sql
+++ database/installDatabase.sql
@@ -9,23 +9,37 @@ CREATE TABLE Cliente (
     sobrenome_cli VARCHAR(50),
     email_cli VARCHAR(100) NOT NULL UNIQUE,
     senha_cli VARCHAR(255) NOT NULL,
-    endereco_cli TEXT
+    cpf_cli VARCHAR(11) NOT NULL,
+    rua VARCHAR(255),
+    numero VARCHAR(10),
+    bairro VARCHAR(100),
+    cidade VARCHAR(100),
 );
 
--- OPÇÕES PARA TIPO DE PESSOA
--- pessoa fisica
-CREATE TABLE Pessoa_Fisica (
-    id_cli INT PRIMARY KEY,
-    cpf_cli VARCHAR(11) NOT NULL UNIQUE,
+CREATE TABLE Servico (
+    id_serv INT AUTO_INCREMENT PRIMARY KEY,
+    id_cli INT NOT NULL,
+    desc_serv VARCHAR(255),
+    valor_serv DECIMAL(10, 2),
+    data_serv DATE,
+    prazo_serv DATE,
     FOREIGN KEY (id_cli) REFERENCES Cliente(id_cli)
 );
 
--- pessoa juridica
-CREATE TABLE Pessoa_Juridica (
-    id_cli INT PRIMARY KEY,
-    cnpj_cli VARCHAR(14) NOT NULL UNIQUE,
-    FOREIGN KEY (id_cli) REFERENCES Cliente(id_cli)
-);
+-- -- OPÇÕES PARA TIPO DE PESSOA
+-- -- pessoa fisica
+-- CREATE TABLE Pessoa_Fisica (
+--     id_cli INT PRIMARY KEY,
+--     cpf_cli VARCHAR(11) NOT NULL UNIQUE,
+--     FOREIGN KEY (id_cli) REFERENCES Cliente(id_cli)
+-- );
+
+-- -- pessoa juridica
+-- CREATE TABLE Pessoa_Juridica (
+--     id_cli INT PRIMARY KEY,
+--     cnpj_cli VARCHAR(14) NOT NULL UNIQUE,
+--     FOREIGN KEY (id_cli) REFERENCES Cliente(id_cli)
+-- );
 
 -- criando a tabela Pedido
 CREATE TABLE Pedido (
@@ -52,10 +66,20 @@ CREATE TABLE Item_Pedido (
     FOREIGN KEY (id_prod) REFERENCES Produto(id_prod)
 );
 
--- criando a tabela Estoque
-CREATE TABLE Estoque (
-    id_estoque INT AUTO_INCREMENT PRIMARY KEY,
-    id_prod INT NOT NULL,
-    quant_dispo INT NOT NULL,
-    FOREIGN KEY (id_prod) REFERENCES Produto(id_prod)
+CREATE TABLE Fornecedor (
+    id_for INT AUTO_INCREMENT PRIMARY KEY,
+    nome_for VARCHAR(50) NOT NULL,
+    cnpj VARCHAR(18) NOT NULL UNIQUE,
+    rua VARCHAR(255),
+    numero VARCHAR(10),
+    bairro VARCHAR(100),
+    cidade VARCHAR(100),
+    email_for VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE Usuario (
+    id_usu INT AUTO_INCREMENT PRIMARY KEY,
+    nome_usu VARCHAR(50),
+    email_usu VARCHAR(255) NOT NULL UNIQUE,
+    senha_usu VARCHAR(255) NOT NULL
 );
